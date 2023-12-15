e569499cd920a083eb994ab0d5b2bb8b03edbe67
zGiusee <giusebelforte@gmail.com>
2023-12-15 13:06:31 +0100

    [UPDATE] added comments

--- js/script.js
+++ js/script.js
@@ -57,7 +57,6 @@ createApp({
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received',
-                            dropdown: false
                         },
                         {
                             date: '20/03/2020 16:35:00',
@@ -76,7 +75,6 @@ createApp({
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
                             status: 'received',
-                            dropdown: false
                         },
                         {
                             date: '28/03/2020 10:20:10',
@@ -88,7 +86,6 @@ createApp({
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
                             status: 'received',
-                            dropdown: false
                         }
                     ],
                 },
@@ -107,7 +104,6 @@ createApp({
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
                             status: 'received',
-                            dropdown: false
                         }
                     ],
                 },
@@ -126,7 +122,6 @@ createApp({
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
                             status: 'received',
-                            dropdown: false
                         }
                     ],
                 },
@@ -145,7 +140,6 @@ createApp({
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
                             status: 'received',
-                            dropdown: false
                         },
                         {
                             date: '10/01/2020 15:51:00',
@@ -170,7 +164,6 @@ createApp({
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
                             status: 'received',
-                            dropdown: false
                         }
                     ],
                 },
@@ -183,7 +176,6 @@ createApp({
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
                             status: 'received',
-                            dropdown: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
@@ -195,7 +187,6 @@ createApp({
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
                             status: 'received',
-                            dropdown: false
                         }
                     ],
                 }
@@ -220,7 +211,7 @@ createApp({
                 date: '10/01/2020 15:50:00',
                 message: this.newMessage,
                 status: 'sent',
-                dropdown: false
+                dropdown: false,
             }
 
             // RISPOSTA =
@@ -228,7 +219,8 @@ createApp({
 
                 date: '10/01/2020 15:50:00',
                 message: 'Ok',
-                status: 'received'
+                status: 'received',
+                dropdown: false,
             }
 
             // MESSAGGIO INVIATO =
@@ -256,9 +248,11 @@ createApp({
         lastMessage(index){
             return this.contacts[index].messages[this.contacts[index].messages.length - 1].message.slice(0,20) + '...';
         },
+        // DEFINISCO LA FUNZIONE CHE RECUPERI LA DATA DELL'ULTIMO MESSAGGIO
         lastDate(index){
             return this.contacts[index].messages[this.contacts[index].messages.length - 1].date;
         },
+        // AGGIUNGO IL DROPDOWN TOGGLE
         showMsgDropdown(index){
             if(this.contacts[this.visibleChat].messages[index].dropdown){
                 this.contacts[this.visibleChat].messages[index].dropdown = false;
@@ -267,8 +261,9 @@ createApp({
                 this.contacts[this.visibleChat].messages[index].dropdown = true;               
             }
         },
+        // DEFINISCO LA FUNZIONE CHE RIMUOVA I MESSAGGI
         deleteMessage(index){
-            this.contacts[this.visibleChat].messages[index].splice(index, 1);
+            this.contacts[this.visibleChat].messages.splice(index, 1);
         }
 
     },
